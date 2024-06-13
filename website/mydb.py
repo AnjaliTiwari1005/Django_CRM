9b2b47bcf791fc18cf16ce37f792052c88949c64
Anjali Tiwari <anjali.1005tiwari@gmail.com>
2024-06-13 16:49:18 +0530

    Initial Commit

---
+++ website/mydb.py
@@ -0,0 +1,12 @@
+#import mysql.connector
+
+ #dataBase = mysql.connector.connect(
+ # host = 'local host',
+ # user = 'root',
+ # password = 'Mapapa98@'
+)
+
+ #cursorObject = dataBase.cursor()
+#cursorObject.execute("CREATE DATABASE CRM_db")
+#print("All Done")
+
